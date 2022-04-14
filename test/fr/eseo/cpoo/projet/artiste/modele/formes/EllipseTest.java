59dd28ed44180f28d91a67eb2d73a4a78a09d1ce
Larson-09 <jordan.laude.9@gmail.com>
2022-04-14 14:17:04 +0200

    add gitignore file

---
+++ test/fr/eseo/cpoo/projet/artiste/modele/formes/EllipseTest.java
@@ -0,0 +1,28 @@
+package fr.eseo.cpoo.projet.artiste.modele.formes;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class EllipseTest {
+	
+	private Ellipse ellipse;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		this.ellipse = new Ellipse();
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		this.ellipse = null;
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
